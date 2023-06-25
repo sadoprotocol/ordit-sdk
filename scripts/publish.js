09884214e40150562929646dd9cbec1cf147d90d
Sai Kranthi <iamsaikranthi@gmail.com>
2023-06-25 18:41:33 +0530

    refactor(workspace): temp remove config workspace versioning

--- scripts/publish.js
+++ scripts/publish.js
@@ -5,15 +5,15 @@ const { readdirSync } = require("node:fs");
 const ROOT_DIR = resolve(__dirname, "..");
 
 const packages = readdirSync(join(ROOT_DIR, "packages"));
-const configs = readdirSync(join(ROOT_DIR, "configs"));
+// const configs = readdirSync(join(ROOT_DIR, "configs"));
 
 async function main() {
   for (const pkg of packages) {
     await publish(join(ROOT_DIR, "packages", pkg));
   }
-  for (const cfg of configs) {
-    await publish(join(ROOT_DIR, "configs", cfg));
-  }
+  // for (const cfg of configs) {
+  //   await publish(join(ROOT_DIR, "configs", cfg));
+  // }
 }
 
 async function publish(cwd) {
