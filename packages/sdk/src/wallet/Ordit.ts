d7ded866fc27a5e5b43d3762d2a6a8fa5b09c943
Sai Kranthi <iamsaikranthi@gmail.com>
2023-06-29 09:59:43 +0530

    feat(sdk): support signing and relaying of tx

--- packages/sdk/src/wallet/Ordit.ts
+++ packages/sdk/src/wallet/Ordit.ts
@@ -4,15 +4,10 @@ import { mnemonicToSeedSync } from "bip39";
 import * as bitcoin from "bitcoinjs-lib";
 import ECPairFactory, { ECPairInterface } from "ecpair";
 
-import {
-  AddressFormats,
-  AddressTypes,
-  addressTypeToName,
-  getAddressesFromPublicKey,
-  getNetwork,
-  hdNodeToChild
-} from "..";
+import { AddressFormats, getAddressesFromPublicKey, getNetwork, hdNodeToChild } from "..";
+import { OrditApi } from "../api";
 import { Network } from "../config/types";
+
 bitcoin.initEccLib(ecc);
 const ECPair = ECPairFactory(ecc);
 const bip32 = BIP32Factory(ecc);
@@ -20,9 +15,10 @@ const bip32 = BIP32Factory(ecc);
 export class Ordit {
   #network: Network = "testnet";
   //   #config;
+  #initialized = false;
   #keyPair: ECPairInterface;
   publicKey: string;
-  allAddresses: ReturnType<typeof getAddressesFromPublicKey> | undefined;
+  allAddresses: ReturnType<typeof getAddressesFromPublicKey> = [];
   selectedAddressType: AddressFormats | undefined;
   selectedAddress: string | undefined;
 
@@ -49,10 +45,6 @@ export class Ordit {
       const child = hdNodeToChild(hdNode, "legacy", 0);
 
       this.#keyPair = ECPair.fromPrivateKey(child.privateKey!, { network: networkObj });
-    } else if (privateKey) {
-      const privateKeyBuffer = Buffer.from(privateKey, "hex");
-
-      this.#keyPair = ECPair.fromPrivateKey(privateKeyBuffer, { network: networkObj });
     } else {
       throw new Error("Invalid options provided.");
     }
@@ -70,13 +62,17 @@ export class Ordit {
     this.#network = value;
   }
 
-  #initialize() {
-    const addresses = this.getAllAddresses();
-    this.allAddresses = addresses;
+  getAddressByType(type: AddressFormats) {
+    if (!this.#initialized || !this.allAddresses.length) {
+      throw new Error("Wallet not fully initialized.");
+    }
+    const result = this.allAddresses.find((address) => address.format === type);
 
-    const addressFormat = addresses[0].format as AddressTypes;
-    this.selectedAddressType = addressTypeToName[addressFormat];
-    this.selectedAddress = addresses[0].address;
+    if (!result) {
+      throw new Error(`Address of type ${type} not found in the instance.`);
+    }
+
+    return result;
   }
 
   getAllAddresses() {
@@ -85,6 +81,100 @@ export class Ordit {
     }
 
     return getAddressesFromPublicKey(this.publicKey, this.#network, "all");
+  }
+
+  setDefaultAddress(type: AddressFormats) {
+    if (this.selectedAddressType === type) return;
+
+    const result = this.getAddressByType(type);
+
+    this.selectedAddress = result.address;
+    this.selectedAddressType = type;
+  }
+
+  signPsbt(hex?: string, base64?: string) {
+    let psbt = null;
+
+    if (!this.#keyPair || !this.#initialized) {
+      throw new Error("Wallet not fully initialized.");
+    }
+    if (!(hex || base64) || (hex && base64)) {
+      throw new Error("Invalid options provided.");
+    }
+
+    if (hex) {
+      psbt = bitcoin.Psbt.fromHex(hex);
+    }
+
+    if (base64) {
+      psbt = bitcoin.Psbt.fromBase64(base64);
+    }
+
+    if (!psbt || !psbt.inputCount) {
+      throw new Error("Invalid PSBT provided.");
+    }
+
+    for (let i = 0; i < psbt.inputCount; i++) {
+      try {
+        psbt.signInput(i, this.#keyPair);
+      } catch (e) {
+        throw new Error(e.message);
+      }
+    }
+
+    const psbtHex = psbt.toHex();
+    const psbtBase64 = psbt.toBase64();
+
+    const psbtHasBeenSigned = psbtHex !== hex || psbtBase64 !== base64;
+
+    if (psbtHasBeenSigned) {
+      try {
+        psbt.finalizeAllInputs();
+
+        const signedHex = psbt.extractTransaction().toHex();
+
+        return {
+          hex: signedHex
+        };
+      } catch (error) {
+        return {
+          hex: psbtHex,
+          base64: psbtBase64
+        };
+      }
+    } else {
+      throw new Error("Signed PSBT is same as input PSBT.");
+    }
+  }
+
+  async relayTx(hex: string, network?: Network) {
+    if (!hex) {
+      throw new Error("Invalid options provided.");
+    }
+
+    const txResponse = await OrditApi.fetch<{ success: boolean; rdata: Array<any> }>("utxo/relay", {
+      data: { hex },
+      network: network ?? this.#network
+    });
+
+    if (!txResponse.success || !txResponse.rdata) {
+      throw new Error("Failed to relay transaction.");
+    }
+
+    return {
+      txid: txResponse.rdata
+    };
+  }
+
+  #initialize() {
+    const addresses = this.getAllAddresses();
+    this.allAddresses = addresses;
+
+    const addressFormat = addresses[0].format as AddressFormats;
+    this.selectedAddressType = addressFormat;
+    this.selectedAddress = addresses[0].address;
+
+    this.#initialized = true;
   }
 }
 
@@ -95,3 +185,5 @@ export type WalletOptions = {
   bip39?: string;
   network?: Network;
 };
+
+export type Address = ReturnType<typeof getAddressesFromPublicKey>[0];
