c1e52a4cc336e1a65732dbea462d0456904c6644
Jordan Kee <jordan.kee@cakedefi.com>
2023-07-13 00:07:16 +0800

    Propagate txId string return to relay.ts

--- packages/sdk/src/transactions/relay.ts
+++ packages/sdk/src/transactions/relay.ts
@@ -2,15 +2,13 @@ import { OrditApi } from "../api";
 import { Network } from "../config/types";
 
 export async function relayTransaction(hex: string, network: Network) {
-  const txResponse = await OrditApi.fetch<{ success: boolean; rdata: Array<any> }>("utxo/relay", {
+  const txResponse = await OrditApi.fetch<{ success: boolean; rdata: string }>("utxo/relay", {
     data: { hex },
     network: network
   });
 
   if (txResponse.success && txResponse.rdata) {
-    return {
-      txid: txResponse.rdata
-    };
+    return txResponse.rdata;
   }
 
   throw new Error("Failed to relay transaction.");
