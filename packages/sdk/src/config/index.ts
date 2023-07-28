1aae2f9723dd65f19210693a8d11949447cc854d
Nishant Ghodke <nishant.ghodke@cakedefi.com>
2023-07-28 13:18:06 +0530

    refactor: update utxo api v2 URL

--- packages/sdk/src/config/index.ts
+++ packages/sdk/src/config/index.ts
@@ -2,17 +2,17 @@ export const apiConfig = {
   version: "0.0.0.10",
   apis: {
     mainnet: {
-      batter: "https://mainnet.ordit.io/",
+      batter: "https://proto.ordit.io/",
       orderbook: "1H4vvBnr62YWQmvNSt8Z4pDw3Vcv1n5xz7",
       ipfs: "http://ipfs-gateway.ordit.io/"
     },
     regtest: {
-      batter: "https://regtest.ordit.io/",
+      batter: "https://regtest-v2.ordit.io/",
       orderbook: "bcrt1q2ys7qws8g072dqe3psp92pqz93ac6wmztexkh5",
       ipfs: "http://ipfs-gateway.ordit.io/"
     },
     testnet: {
-      batter: "https://testnet.ordit.io/",
+      batter: "https://testnet-v2.ordit.io/",
       orderbook: "tb1qfnw26753j7kqu3q099sd48htvtk5wm4e0enmru",
       ipfs: "http://ipfs-gateway.ordit.io/"
     }
