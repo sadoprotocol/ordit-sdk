df0f3562528a265255e64640fed6b1ec23a4ec71
Nishant Ghodke <nishant.ghodke@cakedefi.com>
2023-07-31 19:45:47 +0530

    chore: lint OrdTransaction file

--- packages/sdk/src/transactions/OrdTransaction.ts
+++ packages/sdk/src/transactions/OrdTransaction.ts
@@ -1,6 +1,6 @@
-import * as ecc from "@bitcoinerlab/secp256k1";
-import * as bitcoin from "bitcoinjs-lib";
-import { Tapleaf } from "bitcoinjs-lib/src/types";
+import * as ecc from "@bitcoinerlab/secp256k1"
+import * as bitcoin from "bitcoinjs-lib"
+import { Tapleaf } from "bitcoinjs-lib/src/types"
 
 import {
   buildWitnessScript,
@@ -11,30 +11,30 @@ import {
   GetWalletOptions,
   OnOffUnion,
   OrditApi
-} from "..";
-import { Network } from "../config/types";
+} from ".."
+import { Network } from "../config/types"
 
-bitcoin.initEccLib(ecc);
+bitcoin.initEccLib(ecc)
 
 export class OrdTransaction {
-  publicKey: string;
-  feeRate: number;
-  postage: number;
-  mediaType: string;
-  mediaContent: string;
-  destinationAddress: string;
-  changeAddress: string;
-  meta: object | unknown;
-  network: Network;
-  psbt: bitcoin.Psbt | null = null;
-  ready = false;
-  #xKey: string;
-  #feeForWitnessData: number | null = null;
-  #commitAddress: string | null = null;
-  #inscribePayTx: ReturnType<typeof createTransaction> | null = null;
-  #suitableUnspent: any = null;
-  #recovery = false;
-  #outs: Outputs = [];
+  publicKey: string
+  feeRate: number
+  postage: number
+  mediaType: string
+  mediaContent: string
+  destinationAddress: string
+  changeAddress: string
+  meta: object | unknown
+  network: Network
+  psbt: bitcoin.Psbt | null = null
+  ready = false
+  #xKey: string
+  #feeForWitnessData: number | null = null
+  #commitAddress: string | null = null
+  #inscribePayTx: ReturnType<typeof createTransaction> | null = null
+  #suitableUnspent: any = null
+  #recovery = false
+  #outs: Outputs = []
   #safeMode: OnOffUnion
 
   constructor({
@@ -47,53 +47,53 @@ export class OrdTransaction {
     ...otherOptions
   }: OrdTransactionOptions) {
     if (!publicKey || !otherOptions.changeAddress || !otherOptions.destination || !otherOptions.mediaContent) {
-      throw new Error("Invalid options provided.");
+      throw new Error("Invalid options provided.")
     }
 
-    this.publicKey = publicKey;
-    this.feeRate = feeRate;
-    this.mediaType = mediaType;
-    this.network = network;
-    this.changeAddress = otherOptions.changeAddress;
-    this.destinationAddress = otherOptions.destination;
-    this.mediaContent = otherOptions.mediaContent;
-    this.meta = otherOptions.meta;
-    this.postage = postage;
-    this.#outs = outs;
-    this.#safeMode = !otherOptions.safeMode ? "on": otherOptions.safeMode
+    this.publicKey = publicKey
+    this.feeRate = feeRate
+    this.mediaType = mediaType
+    this.network = network
+    this.changeAddress = otherOptions.changeAddress
+    this.destinationAddress = otherOptions.destination
+    this.mediaContent = otherOptions.mediaContent
+    this.meta = otherOptions.meta
+    this.postage = postage
+    this.#outs = outs
+    this.#safeMode = !otherOptions.safeMode ? "on" : otherOptions.safeMode
 
-    const xKey = getAddressesFromPublicKey(publicKey, network, "p2tr")[0].xkey;
+    const xKey = getAddressesFromPublicKey(publicKey, network, "p2tr")[0].xkey
 
     if (!xKey) {
-      throw new Error("Failed to derive xKey from the provided public key.");
+      throw new Error("Failed to derive xKey from the provided public key.")
     }
 
-    this.#xKey = xKey;
+    this.#xKey = xKey
   }
 
   get outs() {
-    return this.#outs;
+    return this.#outs
   }
 
   build() {
     if (!this.#suitableUnspent || !this.#inscribePayTx) {
-      throw new Error("Failed to build PSBT. Transaction not ready.");
+      throw new Error("Failed to build PSBT. Transaction not ready.")
     }
 
-    let fees = this.#feeForWitnessData!;
+    let fees = this.#feeForWitnessData!
 
     if (this.#recovery) {
-      fees = calculateTxFeeWithRate(1, 0, this.feeRate, 1);
+      fees = calculateTxFeeWithRate(1, 0, this.feeRate, 1)
     }
 
     const customOutsAmount = this.#outs.reduce((acc, cur) => {
-      return acc + cur.value;
-    }, 0);
-    const change = this.#suitableUnspent.sats - fees - customOutsAmount - this.postage;
+      return acc + cur.value
+    }, 0)
+    const change = this.#suitableUnspent.sats - fees - customOutsAmount - this.postage
 
-    const networkObj = getNetwork(this.network);
+    const networkObj = getNetwork(this.network)
 
-    const psbt = new bitcoin.Psbt({ network: networkObj });
+    const psbt = new bitcoin.Psbt({ network: networkObj })
 
     try {
       psbt.addInput({
@@ -112,59 +112,59 @@ export class OrdTransaction {
             controlBlock: this.#inscribePayTx.witness![this.#inscribePayTx.witness!.length - 1]
           }
         ]
-      });
+      })
 
       if (!this.#recovery) {
         psbt.addOutput({
           address: this.destinationAddress,
           value: this.postage
-        });
+        })
 
         this.#outs.forEach((out) => {
-          psbt.addOutput(out);
-        });
+          psbt.addOutput(out)
+        })
       }
 
       if (change > 600) {
-        let changeAddress = this.#inscribePayTx.address;
+        let changeAddress = this.#inscribePayTx.address
         if (this.changeAddress) {
-          changeAddress = this.changeAddress;
+          changeAddress = this.changeAddress
         }
 
         psbt.addOutput({
           address: changeAddress!,
           value: change
-        });
+        })
       }
 
-      this.psbt = psbt;
+      this.psbt = psbt
     } catch (error) {
-      throw new Error(error.message);
+      throw new Error(error.message)
     }
   }
 
   toPsbt() {
     if (!this.psbt) {
-      throw new Error("No PSBT found. Please build first.");
+      throw new Error("No PSBT found. Please build first.")
     }
 
-    return this.psbt;
+    return this.psbt
   }
 
   toHex() {
     if (!this.psbt) {
-      throw new Error("No PSBT found. Please build first.");
+      throw new Error("No PSBT found. Please build first.")
     }
 
-    return this.psbt.toHex();
+    return this.psbt.toHex()
   }
 
   toBase64() {
     if (!this.psbt) {
-      throw new Error("No PSBT found. Please build first.");
+      throw new Error("No PSBT found. Please build first.")
     }
 
-    return this.psbt.toBase64();
+    return this.psbt.toBase64()
   }
 
   generateCommit() {
@@ -173,17 +173,17 @@ export class OrdTransaction {
       mediaType: this.mediaType,
       meta: this.meta,
       xkey: this.#xKey
-    });
+    })
     const recoverScript = buildWitnessScript({
       mediaContent: this.mediaContent,
       mediaType: this.mediaType,
       meta: this.meta,
       xkey: this.#xKey,
       recover: true
-    });
+    })
 
     if (!witnessScript || !recoverScript) {
-      throw new Error("Failed to build createRevealPsbt");
+      throw new Error("Failed to build createRevealPsbt")
     }
 
     const scriptTree: [Tapleaf, Tapleaf] = [
@@ -193,38 +193,38 @@ export class OrdTransaction {
       {
         output: recoverScript
       }
-    ];
+    ]
 
     const redeemScript = {
       output: witnessScript,
       redeemVersion: 192
-    };
+    }
 
     const inscribePayTx = createTransaction(Buffer.from(this.#xKey, "hex"), "p2tr", this.network, {
       scriptTree: scriptTree,
       redeem: redeemScript
-    });
+    })
 
-    const fees = JSON.parse(JSON.stringify((80 + 1 * 180) * this.feeRate));
-    const scriptLength = witnessScript.toString("hex").length;
-    const scriptFees = Math.ceil((scriptLength / 10) * this.feeRate + fees);
+    const fees = JSON.parse(JSON.stringify((80 + 1 * 180) * this.feeRate))
+    const scriptLength = witnessScript.toString("hex").length
+    const scriptFees = Math.ceil((scriptLength / 10) * this.feeRate + fees)
     const customOutsAmount = this.#outs.reduce((acc, cur) => {
-      return acc + cur.value;
-    }, 0);
+      return acc + cur.value
+    }, 0)
 
-    this.#feeForWitnessData = scriptFees;
-    this.#commitAddress = inscribePayTx.address!;
-    this.#inscribePayTx = inscribePayTx;
+    this.#feeForWitnessData = scriptFees
+    this.#commitAddress = inscribePayTx.address!
+    this.#inscribePayTx = inscribePayTx
 
     return {
       address: inscribePayTx.address!,
       revealFee: this.postage + scriptFees + customOutsAmount
-    };
+    }
   }
 
   recover() {
     if (!this.#inscribePayTx || !this.ready) {
-      throw new Error("Transaction not ready.");
+      throw new Error("Transaction not ready.")
     }
 
     const witnessScript = buildWitnessScript({
@@ -232,17 +232,17 @@ export class OrdTransaction {
       mediaType: this.mediaType,
       meta: this.meta,
       xkey: this.#xKey
-    });
+    })
     const recoverScript = buildWitnessScript({
       mediaContent: this.mediaContent,
       mediaType: this.mediaType,
       meta: this.meta,
       xkey: this.#xKey,
       recover: true
-    });
+    })
 
     if (!witnessScript || !recoverScript) {
-      throw new Error("Failed to build createRevealPsbt");
+      throw new Error("Failed to build createRevealPsbt")
     }
 
     const scriptTree: [Tapleaf, Tapleaf] = [
@@ -252,79 +252,83 @@ export class OrdTransaction {
       {
         output: recoverScript
       }
-    ];
+    ]
 
     const redeemScript = {
       output: recoverScript,
       redeemVersion: 192
-    };
+    }
 
     const inscribePayTx = createTransaction(Buffer.from(this.#xKey, "hex"), "p2tr", this.network, {
       scriptTree: scriptTree,
       redeem: redeemScript
-    });
+    })
 
-    this.#inscribePayTx = inscribePayTx;
-    this.#recovery = true;
+    this.#inscribePayTx = inscribePayTx
+    this.#recovery = true
   }
 
   async isReady() {
     if (!this.#commitAddress || !this.#feeForWitnessData) {
-      throw new Error("No commit address found. Please generate a commit address.");
+      throw new Error("No commit address found. Please generate a commit address.")
     }
 
     if (!this.ready) {
       try {
-        await this.fetchAndSelectSuitableUnspent();
+        await this.fetchAndSelectSuitableUnspent()
       } catch (error) {
-        return false;
+        return false
       }
     }
 
-    return this.ready;
+    return this.ready
   }
 
   async fetchAndSelectSuitableUnspent() {
     if (!this.#commitAddress || !this.#feeForWitnessData) {
-      throw new Error("No commit address found. Please generate a commit address.");
+      throw new Error("No commit address found. Please generate a commit address.")
     }
 
-    const { spendableUTXOs } = await OrditApi.fetchUnspentUTXOs({ 
-      address: this.#commitAddress, network: this.network,
+    const { spendableUTXOs } = await OrditApi.fetchUnspentUTXOs({
+      address: this.#commitAddress,
+      network: this.network
     })
 
     const customOutsAmount = this.#outs.reduce((acc, cur) => {
-      return acc + cur.value;
-    }, 0);
+      return acc + cur.value
+    }, 0)
 
     const suitableUTXO = spendableUTXOs.find((utxo) => {
-      if (utxo.sats >= this.postage + this.#feeForWitnessData! + customOutsAmount && (this.#safeMode === 'off' || (this.#safeMode === 'on' && utxo.safeToSpend === true))) {
-        return true;
+      if (
+        utxo.sats >= this.postage + this.#feeForWitnessData! + customOutsAmount &&
+        (this.#safeMode === "off" || (this.#safeMode === "on" && utxo.safeToSpend === true))
+      ) {
+        return true
       }
-    }, this);
+    }, this)
 
     if (!suitableUTXO) {
-      throw new Error("No suitable unspent found for reveal.");
+      throw new Error("No suitable unspent found for reveal.")
     }
 
-    this.#suitableUnspent = suitableUTXO;
-    this.ready = true;
+    this.#suitableUnspent = suitableUTXO
+    this.ready = true
 
-    return suitableUTXO;
+    return suitableUTXO
   }
 }
 
-export type OrdTransactionOptions = Pick<GetWalletOptions, 'safeMode'> & {
-  feeRate?: number;
-  postage?: number;
-  mediaType?: string;
-  mediaContent: string;
-  destination: string;
-  changeAddress: string;
-  meta?: object | unknown;
-  network?: Network;
-  publicKey: string;
-  outs?: Outputs;
-};
+export type OrdTransactionOptions = Pick<GetWalletOptions, "safeMode"> & {
+  feeRate?: number
+  postage?: number
+  mediaType?: string
+  mediaContent: string
+  destination: string
+  changeAddress: string
+  meta?: object | unknown
+  network?: Network
+  publicKey: string
+  outs?: Outputs
+}
 
-type Outputs = Array<{ address: string; value: number }>;
+type Outputs = Array<{ address: string; value: number }>
