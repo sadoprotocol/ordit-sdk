12a72b587158c47c0b737587f47d67e2a1d54236
Nishant Ghodke <nishant.ghodke@cakedefi.com>
2023-07-31 21:13:49 +0530

    chore: lint psbt.ts file

--- packages/sdk/src/transactions/psbt.ts
+++ packages/sdk/src/transactions/psbt.ts
@@ -1,9 +1,9 @@
-import * as ecc from "@bitcoinerlab/secp256k1";
-import BIP32Factory, { BIP32API } from "bip32";
-import { Network, Psbt } from "bitcoinjs-lib";
+import * as ecc from "@bitcoinerlab/secp256k1"
+import BIP32Factory, { BIP32API } from "bip32"
+import { Network, Psbt } from "bitcoinjs-lib"
 
-import { createTransaction, getNetwork } from "../utils";
-import { GetWalletOptions, getWalletWithBalances } from "../wallet";
+import { createTransaction, getNetwork } from "../utils"
+import { GetWalletOptions, getWalletWithBalances } from "../wallet"
 
 export async function createPsbt({
   network,
@@ -14,124 +14,124 @@ export async function createPsbt({
   safeMode = "on",
   satsPerByte
 }: CreatePsbtOptions) {
-  const netWorkObj = getNetwork(network);
-  const bip32 = BIP32Factory(ecc);
+  const netWorkObj = getNetwork(network)
+  const bip32 = BIP32Factory(ecc)
 
   const walletWithBalances = await getWalletWithBalances({
     pubKey,
     format,
     network,
     safeMode
-  });
+  })
 
   if (walletWithBalances?.spendables === undefined) {
     throw new Error(
       "The derived wallet doesn't contain any spendable sats. It may be empty or contain sats that are either linked to inscriptions or tied to ordinals."
-    );
+    )
   }
 
-  let fees = 0;
-  let change = 0;
-  const dust = 600;
-  let inputs_used = 0;
-  const sats_per_byte = satsPerByte ?? 10;
-  let total_cardinals_to_send = 0;
-  let total_cardinals_available = 0;
-  const unsupported_inputs = [];
-  const unspents_to_use = [];
-  const xverse_inputs = [];
+  let fees = 0
+  let change = 0
+  const dust = 600
+  let inputs_used = 0
+  const sats_per_byte = satsPerByte ?? 10
+  let total_cardinals_to_send = 0
+  let total_cardinals_available = 0
+  const unsupported_inputs = []
+  const unspents_to_use = []
+  const xverse_inputs = []
 
-  const psbt = new Psbt({ network: netWorkObj });
+  const psbt = new Psbt({ network: netWorkObj })
 
   outs.forEach((output) => {
     try {
-      if (!output.cardinals) throw new Error("No cardinals in output.");
+      if (!output.cardinals) throw new Error("No cardinals in output.")
 
-      total_cardinals_to_send = +parseInt(output.cardinals);
+      total_cardinals_to_send = +parseInt(output.cardinals)
       psbt.addOutput({
         address: output.address,
         value: parseInt(output.cardinals)
-      });
+      })
     } catch (error) {
       //handle error
     }
-  });
+  })
 
   ins.forEach((input, idx) => {
     if (input.address) {
       walletWithBalances.spendables.forEach((spendable: any) => {
-        const sats = spendable.sats;
-        const scriptPubKeyAddress = spendable.scriptPubKey.address;
-        const scriptPubKeyType = spendable.scriptPubKey.type as string;
-        let addedInputSuccessfully = false;
+        const sats = spendable.sats
+        const scriptPubKeyAddress = spendable.scriptPubKey.address
+        const scriptPubKeyType = spendable.scriptPubKey.type as string
+        let addedInputSuccessfully = false
 
-        fees = JSON.parse(JSON.stringify((80 + (inputs_used + 1) * 180) * sats_per_byte));
+        fees = JSON.parse(JSON.stringify((80 + (inputs_used + 1) * 180) * sats_per_byte))
 
         if (input.address === "any") {
-          ins[idx].address = scriptPubKeyAddress;
+          ins[idx].address = scriptPubKeyAddress
         }
 
         if (input.address === scriptPubKeyAddress) {
-          addedInputSuccessfully = addInputToPsbtByType(spendable, scriptPubKeyType, psbt, bip32, netWorkObj);
+          addedInputSuccessfully = addInputToPsbtByType(spendable, scriptPubKeyType, psbt, bip32, netWorkObj)
 
           if (addedInputSuccessfully) {
-            unspents_to_use.push(spendable);
-            total_cardinals_available += sats;
+            unspents_to_use.push(spendable)
+            total_cardinals_available += sats
             xverse_inputs.push({
               address: scriptPubKeyAddress,
               signingIndexes: [inputs_used]
-            });
-            inputs_used++;
+            })
+            inputs_used++
           } else {
-            unsupported_inputs.push(spendable);
+            unsupported_inputs.push(spendable)
           }
         }
-      });
+      })
     }
-  });
+  })
 
   if (!unspents_to_use.length) {
     throw new Error(
       `Not enough input value to cover outputs and fees. Total cardinals available: '${total_cardinals_available}'. Cardinals to send: '${total_cardinals_to_send}'. Estimated fees: '${fees}'.`
-    );
+    )
   }
 
-  change = total_cardinals_available - (total_cardinals_to_send + fees);
+  change = total_cardinals_available - (total_cardinals_to_send + fees)
 
   if (change < 0) {
-    throw new Error(`Insufficient balance for tx. Deposit ${change * -1} sats or adjust transfer amount to proceed`);
+    throw new Error(`Insufficient balance for tx. Deposit ${change * -1} sats or adjust transfer amount to proceed`)
   }
 
   if (change >= dust) {
     psbt.addOutput({
       address: ins[0].address,
       value: change
-    });
+    })
   }
 
-  const psbtHex = psbt.toHex();
-  const psbtBase64 = psbt.toBase64();
+  const psbtHex = psbt.toHex()
+  const psbtBase64 = psbt.toBase64()
 
   return {
     hex: psbtHex,
     base64: psbtBase64
-  };
+  }
 }
 
 function addInputToPsbtByType(spendable: any, type: string, psbt: Psbt, bip32: BIP32API, network: Network) {
   if (type === "witness_v1_taproot") {
-    const chainCode = Buffer.alloc(32);
-    chainCode.fill(1);
+    const chainCode = Buffer.alloc(32)
+    chainCode.fill(1)
 
-    let childNodeXOnlyPubkey = Buffer.from(spendable.pub, "hex");
+    let childNodeXOnlyPubkey = Buffer.from(spendable.pub, "hex")
     try {
-      const key = bip32.fromPublicKey(Buffer.from(spendable.pub, "hex"), chainCode, network);
-      childNodeXOnlyPubkey = key.publicKey.subarray(1, 33);
+      const key = bip32.fromPublicKey(Buffer.from(spendable.pub, "hex"), chainCode, network)
+      childNodeXOnlyPubkey = key.publicKey.subarray(1, 33)
     } catch (error) {
       // fail silently
     }
 
-    const p2tr = createTransaction(childNodeXOnlyPubkey, "p2tr", network);
+    const p2tr = createTransaction(childNodeXOnlyPubkey, "p2tr", network)
 
     if (p2tr && p2tr.output) {
       psbt.addInput({
@@ -142,13 +142,13 @@ function addInputToPsbtByType(spendable: any, type: string, psbt: Psbt, bip32: B
           script: p2tr.output,
           value: parseInt(spendable.sats)
         }
-      });
+      })
 
-      return true;
+      return true
     }
   } else if (type === "witness_v0_keyhash") {
     try {
-      const p2wpkh = createTransaction(Buffer.from(spendable.pub, "hex"), "p2wpkh", network);
+      const p2wpkh = createTransaction(Buffer.from(spendable.pub, "hex"), "p2wpkh", network)
 
       if (p2wpkh && p2wpkh.output) {
         psbt.addInput({
@@ -158,16 +158,16 @@ function addInputToPsbtByType(spendable: any, type: string, psbt: Psbt, bip32: B
             script: p2wpkh.output,
             value: parseInt(spendable.sats)
           }
-        });
+        })
       }
 
-      return true;
+      return true
     } catch (e) {
       //fail silently
     }
   } else if (type === "scripthash") {
     try {
-      const p2sh = createTransaction(Buffer.from(spendable.pub, "hex"), "p2sh", network);
+      const p2sh = createTransaction(Buffer.from(spendable.pub, "hex"), "p2sh", network)
 
       if (p2sh && p2sh.output && p2sh.redeem) {
         psbt.addInput({
@@ -178,9 +178,9 @@ function addInputToPsbtByType(spendable: any, type: string, psbt: Psbt, bip32: B
             script: p2sh.output,
             value: parseInt(spendable.sats)
           }
-        });
+        })
 
-        return true;
+        return true
       }
     } catch (error) {
       //fail silently
@@ -191,19 +191,19 @@ function addInputToPsbtByType(spendable: any, type: string, psbt: Psbt, bip32: B
         hash: spendable.txid,
         index: parseInt(spendable.n),
         nonWitnessUtxo: Buffer.from(spendable.txhex, "hex")
-      });
+      })
 
-      return true;
+      return true
     } catch (e) {
       //fail silently
     }
   }
 
-  return false;
+  return false
 }
 
 export type CreatePsbtOptions = GetWalletOptions & {
-  satsPerByte?: number;
-  ins: any[];
-  outs: any[];
-};
+  satsPerByte?: number
+  ins: any[]
+  outs: any[]
+}
