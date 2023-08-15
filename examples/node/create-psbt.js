c44f8ac69f48a17e83346e0f46e47de2ba897e40
Nishant Ghodke <nishant.ghodke@cakedefi.com>
2023-08-15 17:17:44 +0530

    chore: update create-psbt example

--- examples/node/create-psbt.js
+++ examples/node/create-psbt.js
@@ -11,10 +11,8 @@ async function main() {
 
     const psbt = await ordit.transactions.createPsbt({
         pubKey: '039ce27aa7666731648421004ba943b90b8273e23a175d9c58e3ec2e643a9b01d1',
-        ins: [{
-            address: 'tb1p98dv6f5jp5qr4z2dtaljvwrhq34xrr8zuaqgv4ajf36vg2mmsruqt5m3lv'
-        }],
-        outs: [{
+        address: 'tb1p98dv6f5jp5qr4z2dtaljvwrhq34xrr8zuaqgv4ajf36vg2mmsruqt5m3lv',
+        outputs: [{
             address: 'tb1qatkgzm0hsk83ysqja5nq8ecdmtwl73zwurawww',
             cardinals: 1200
         }],
