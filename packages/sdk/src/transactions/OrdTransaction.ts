1fb183f085eeb7046a972bf1fb7a2f6cb2f282e5
KevinK <kevin.kihata@gmail.com>
2023-08-25 18:22:47 +0800

    feat: fix fee estimate output + add naiive estimate fee fn

--- packages/sdk/src/transactions/OrdTransaction.ts
+++ packages/sdk/src/transactions/OrdTransaction.ts
@@ -5,6 +5,7 @@ import { Tapleaf } from "bitcoinjs-lib/src/types"
 import {
   buildWitnessScript,
   calculateTxFee,
+  calculateTxVirtualSize,
   convertSatoshisToBTC,
   createTransaction,
   encodeObject,
@@ -219,10 +220,10 @@ export class OrdTransaction {
       redeem: redeemScript
     })
 
-    // inscription tx always have 1 input and 1 output
+    // inscription tx always have 1 input and 1 + n custom outs
     const fees = calculateTxFee({
       totalInputs: 1,
-      totalOutputs: 1,
+      totalOutputs: 1 + this.#outs.length,
       satsPerByte: this.feeRate,
       type: "taproot", // hardcoding because this process is only supported by Taproot txs
       additional: { witnessScripts: inscribePayTx.witness }
@@ -328,6 +329,42 @@ export class OrdTransaction {
     this.ready = true
 
     return suitableUTXO
+  }
+
+  /**
+   * naiive implementation of estimate reveal network fee that does not need the actual content bytes (only size)
+   * @param contentSize
+   * @param contentType
+   * @param meta
+   * @param feeRate
+   */
+  static estimateRevealNetworkFee({
+    contentSize,
+    contentType,
+    meta,
+    feeRate
+  }: {
+    contentSize: number
+    contentType: string
+    meta: any
+    feeRate: number
+  }) {
+    const META_OP_CODES_BYTES_LENGTH = 39 // opcodes
+    const BASE_INSCRIPTION_BYTES_LENGTH = 33 + 10 // 33 (pub key length) + 10 (opcodes)
+    const RECOVER_SCRIPT_BYTES_LENGTH = 34
+
+    const contentTypeLength = Buffer.from(contentType, "utf8").length
+    const metaBytesLength = Buffer.from(JSON.stringify(meta), "utf8").length
+    const estimatedWitnessScriptSize =
+      metaBytesLength +
+      contentSize +
+      META_OP_CODES_BYTES_LENGTH +
+      BASE_INSCRIPTION_BYTES_LENGTH +
+      contentTypeLength +
+      RECOVER_SCRIPT_BYTES_LENGTH
+    const estimatedNetworkFee = Math.ceil((estimatedWitnessScriptSize / 4 + 175) * feeRate)
+
+    return estimatedNetworkFee
   }
 }
 
