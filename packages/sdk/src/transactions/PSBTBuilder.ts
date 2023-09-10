229e88a2f36c427d3582bf3bbe77b7295ede2a06
Nishant Ghodke <nishant.ghodke@cakedefi.com>
2023-09-10 12:11:47 +0530

    refactor: remove log

--- packages/sdk/src/transactions/PSBTBuilder.ts
+++ packages/sdk/src/transactions/PSBTBuilder.ts
@@ -161,13 +161,6 @@ export class PSBTBuilder {
 
   private async retrieveUTXOs() {
     const amount = this.changeAmount < 0 ? this.changeAmount * -1 : this.outputAmount
-    console.log({
-      address: this.address,
-      value: convertSatoshisToBTC(amount),
-      network: this.network,
-      filter: this.getReservedUTXOs()
-    })
-
     const utxos = await OrditApi.fetchSpendables({
       address: this.address,
       value: convertSatoshisToBTC(amount),
