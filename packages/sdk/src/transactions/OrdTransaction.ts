d5990eb7fd582378c6f625ec538185bd835f57f4
Nishant Ghodke <nishant.ghodke@cakedefi.com>
2023-09-12 12:54:43 +0530

    refactor: initial integration w/ PSBTBuilder

--- packages/sdk/src/transactions/OrdTransaction.ts
+++ packages/sdk/src/transactions/OrdTransaction.ts
@@ -4,7 +4,6 @@ import { Tapleaf } from "bitcoinjs-lib/src/types"
 
 import {
   buildWitnessScript,
-  convertSatoshisToBTC,
   createTransaction,
   encodeObject,
   getAddressesFromPublicKey,
@@ -12,54 +11,66 @@ import {
   getNetwork,
   GetWalletOptions,
   OnOffUnion,
-  OrditApi
+  PSBTBuilder
 } from ".."
 import { Network } from "../config/types"
 import { MINIMUM_AMOUNT_IN_SATS } from "../constants"
-import FeeEstimator from "../fee/FeeEstimator"
 import { InputsToSign } from "../inscription/types"
 import { NestedObject } from "../utils/types"
+import { UTXOLimited } from "./types"
 
 bitcoin.initEccLib(ecc)
 
-export class OrdTransaction {
-  publicKey: string
-  feeRate: number
-  postage: number
+export class OrdTransaction extends PSBTBuilder {
+  network: Network
+
   mediaType: string
   mediaContent: string
+  meta?: NestedObject
+  postage: number
+
+  address: string
+  publicKey: string
   destinationAddress: string
   changeAddress: string
-  meta?: NestedObject
-  network: Network
-  psbt: bitcoin.Psbt | null = null
-  ready = false
-  address: string
   inputsToSign: InputsToSign
-  #xKey: string
-  #feeForWitnessData: number | null = null
-  #commitAddress: string | null = null
-  #inscribePayTx: ReturnType<typeof createTransaction> | null = null
-  #suitableUnspent: any = null
-  #recovery = false
-  #outs: Outputs = []
-  #safeMode: OnOffUnion
-  #encodeMetadata: boolean
-  #enableRBF: boolean
+
+  private ready = false
+
+  private xKey!: string
+  private commitAddress: string | null = null
+  private payment: bitcoin.payments.Payment | null = null
+  private suitableUnspent: UTXOLimited | null = null
+  private recovery = false
+  private safeMode: OnOffUnion
+  private encodeMetadata: boolean
+
+  private witnessScripts: Record<"inscription" | "recovery", Buffer | null> = {
+    inscription: null,
+    recovery: null
+  }
+  private taprootTree!: [Tapleaf, Tapleaf]
 
   constructor({
-    feeRate = 10,
-    postage = 10000,
+    feeRate,
+    postage,
     mediaType = "text/plain;charset=utf-8",
     network = "testnet",
     publicKey,
     outs = [],
     encodeMetadata = false,
-    enableRBF = true,
     ...otherOptions
   }: OrdTransactionOptions) {
+    const { xkey, address } = getAddressesFromPublicKey(publicKey, network, "p2tr")[0]
+    super({
+      address: address!,
+      feeRate,
+      network,
+      publicKey,
+      outputs: outs
+    })
     if (!publicKey || !otherOptions.changeAddress || !otherOptions.destination || !otherOptions.mediaContent) {
-      throw new Error("Invalid options provided.")
+      throw new Error("Invalid options provided")
     }
 
     this.publicKey = publicKey
@@ -71,18 +82,15 @@ export class OrdTransaction {
     this.mediaContent = otherOptions.mediaContent
     this.meta = otherOptions.meta
     this.postage = postage
-    this.#outs = outs
-    this.#safeMode = !otherOptions.safeMode ? "on" : otherOptions.safeMode
-    this.#encodeMetadata = encodeMetadata
-    this.#enableRBF = enableRBF
-
-    const { xkey, address } = getAddressesFromPublicKey(publicKey, network, "p2tr")[0]
+    this.outputs = outs
+    this.safeMode = !otherOptions.safeMode ? "on" : otherOptions.safeMode
+    this.encodeMetadata = encodeMetadata
 
     if (!xkey) {
-      throw new Error("Failed to derive xKey from the provided public key.")
+      throw new Error("Failed to derive xKey from the provided public key")
     }
 
-    this.#xKey = xkey
+    this.xKey = xkey
     this.address = address!
     this.inputsToSign = {
       address: this.address,
@@ -91,217 +99,155 @@ export class OrdTransaction {
   }
 
   get outs() {
-    return this.#outs
+    return this.outputs
   }
 
-  build() {
-    if (!this.#suitableUnspent || !this.#inscribePayTx) {
-      throw new Error("Failed to build PSBT. Transaction not ready.")
+  private getMetadata() {
+    return this.meta && this.encodeMetadata ? encodeObject(this.meta) : this.meta
+  }
+
+  async build() {
+    if (!this.suitableUnspent || !this.payment) {
+      throw new Error("Failed to build PSBT. Transaction not ready")
     }
 
     const networkObj = getNetwork(this.network)
-
     const psbt = new bitcoin.Psbt({ network: networkObj })
 
     psbt.addInput({
-      sequence: this.#enableRBF ? 0xfffffffd : 0xffffffff,
-      hash: this.#suitableUnspent.txid,
-      index: parseInt(this.#suitableUnspent.n),
-      tapInternalKey: Buffer.from(this.#xKey, "hex"),
+      sequence: this.getInputSequence(),
+      hash: this.suitableUnspent.txid,
+      index: this.suitableUnspent.n,
+      tapInternalKey: Buffer.from(this.xKey, "hex"),
       witnessUtxo: {
-        script: this.#inscribePayTx.output!,
-        value: parseInt(this.#suitableUnspent.sats)
+        script: this.payment.output!,
+        value: this.suitableUnspent.sats
       },
       tapLeafScript: [
         {
-          leafVersion: this.#inscribePayTx.redeemVersion!,
-          script: this.#inscribePayTx.redeem!.output!,
-          controlBlock: this.#inscribePayTx.witness![this.#inscribePayTx.witness!.length - 1]
+          leafVersion: this.payment.redeemVersion!,
+          script: this.payment.redeem!.output!,
+          controlBlock: this.payment.witness![this.payment.witness!.length - 1]
         }
       ]
     })
 
     this.inputsToSign.signingIndexes.push(0) // hardcoding because there will always be one input
 
-    if (!this.#recovery) {
-      psbt.addOutput({
+    if (!this.recovery) {
+      this.outputs.push({
         address: this.destinationAddress,
         value: this.postage
       })
-
-      this.#outs.forEach((out) => {
-        psbt.addOutput(out)
-      })
     }
 
-    let fee = this.#feeForWitnessData!
-    if (this.#recovery) {
-      const feeEstimator = new FeeEstimator({
-        psbt,
-        feeRate: this.feeRate,
-        network: this.network
-      })
-      fee = feeEstimator.calculateNetworkFee()
+    this.changeAmount = this.suitableUnspent.sats - this.fee - this.outputAmount - this.postage
+    if (this.changeAmount > MINIMUM_AMOUNT_IN_SATS) {
+      await this.addChangeOutput()
     }
 
-    const customOutsAmount = this.#outs.reduce((acc, cur) => {
-      return acc + cur.value
-    }, 0)
-    const change = this.#suitableUnspent.sats - fee - customOutsAmount - this.postage
-
-    if (change > MINIMUM_AMOUNT_IN_SATS) {
-      let changeAddress = this.#inscribePayTx.address
-      if (this.changeAddress) {
-        changeAddress = this.changeAddress
-      }
-
-      psbt.addOutput({
-        address: changeAddress!,
-        value: change
-      })
-    }
+    await this.prepare() // prepare PSBT using PSBTBuilder
 
     this.psbt = psbt
   }
 
-  toPsbt() {
-    if (!this.psbt) {
-      throw new Error("No PSBT found. Please build first.")
+  private isBuilt() {
+    if (!this.commitAddress || !this.fee) {
+      throw new Error("Invalid tx! Make sure you generate commit address or recover and finally build")
     }
-
-    return this.psbt
   }
 
-  toHex() {
-    if (!this.psbt) {
-      throw new Error("No PSBT found. Please build first.")
+  buildWitness() {
+    this.witnessScripts = {
+      inscription: buildWitnessScript({
+        mediaContent: this.mediaContent,
+        mediaType: this.mediaType,
+        meta: this.getMetadata(),
+        xkey: this.xKey
+      }),
+      recovery: buildWitnessScript({
+        mediaContent: this.mediaContent,
+        mediaType: this.mediaType,
+        meta: this.getMetadata(),
+        xkey: this.xKey,
+        recover: true
+      })
     }
-
-    return this.psbt.toHex()
   }
 
-  toBase64() {
-    if (!this.psbt) {
-      throw new Error("No PSBT found. Please build first.")
-    }
+  buildTaprootTree() {
+    this.buildWitness()
 
-    return this.psbt.toBase64()
+    this.taprootTree = [{ output: this.witnessScripts.inscription! }, { output: this.witnessScripts.recovery! }]
+  }
+
+  getInscriptionRedeemScript(): bitcoin.payments.Payment["redeem"] {
+    return {
+      output: this.witnessScripts.inscription!,
+      redeemVersion: 192
+    }
+  }
+
+  getRecoveryRedeemScript(): bitcoin.payments.Payment["redeem"] {
+    return {
+      output: this.witnessScripts.recovery!,
+      redeemVersion: 192
+    }
+  }
+
+  private preview({ activate }: Record<"activate", boolean> = { activate: true }) {
+    if (activate) {
+      this.suitableUnspent = getDummyP2TRInput()
+      this.build()
+    } else {
+      this.initPSBT()
+      this.suitableUnspent = null
+      this.inputsToSign.signingIndexes.pop() // remove last added index
+    }
   }
 
   generateCommit() {
-    const witnessScript = buildWitnessScript({
-      mediaContent: this.mediaContent,
-      mediaType: this.mediaType,
-      meta: this.meta && this.#encodeMetadata ? encodeObject(this.meta) : this.meta,
-      xkey: this.#xKey
-    })
-    const recoverScript = buildWitnessScript({
-      mediaContent: this.mediaContent,
-      mediaType: this.mediaType,
-      meta: this.meta && this.#encodeMetadata ? encodeObject(this.meta) : this.meta,
-      xkey: this.#xKey,
-      recover: true
+    this.buildTaprootTree()
+    const payment = bitcoin.payments.p2tr({
+      internalPubkey: Buffer.from(this.xKey, "hex"),
+      network: getNetwork(this.network),
+      scriptTree: this.taprootTree,
+      redeem: this.getInscriptionRedeemScript()
     })
 
-    if (!witnessScript || !recoverScript) {
-      throw new Error("Failed to build createRevealPsbt")
-    }
+    this.preview()
 
-    const scriptTree: [Tapleaf, Tapleaf] = [
-      {
-        output: witnessScript
-      },
-      {
-        output: recoverScript
-      }
-    ]
+    this.calculateNetworkFee()
 
-    const redeemScript = {
-      output: witnessScript,
-      redeemVersion: 192
-    }
+    this.preview({ activate: false })
 
-    const inscribePayTx = createTransaction(Buffer.from(this.#xKey, "hex"), "p2tr", this.network, {
-      scriptTree: scriptTree,
-      redeem: redeemScript
-    })
-
-    this.#suitableUnspent = getDummyP2TRInput()
-    this.build()
-    const feeEstimator = new FeeEstimator({
-      psbt: this.psbt!,
-      feeRate: this.feeRate,
-      network: this.network
-    })
-    const fee = feeEstimator.calculateNetworkFee()
-    this.psbt = null
-    this.#suitableUnspent = null
-    this.inputsToSign.signingIndexes.pop() // remove last added index
-
-    const customOutsAmount = this.#outs.reduce((acc, cur) => {
-      return acc + cur.value
-    }, 0)
-
-    this.#feeForWitnessData = fee
-    this.#commitAddress = inscribePayTx.address!
-    this.#inscribePayTx = inscribePayTx
+    this.commitAddress = payment.address!
+    this.payment = payment
 
     return {
-      address: inscribePayTx.address!,
-      revealFee: this.postage + fee + customOutsAmount
+      address: payment.address!,
+      revealFee: this.postage + this.fee + this.outputAmount
     }
   }
 
   recover() {
-    if (!this.#inscribePayTx || !this.ready) {
-      throw new Error("Transaction not ready.")
-    }
+    this.buildTaprootTree()
 
-    const witnessScript = buildWitnessScript({
-      mediaContent: this.mediaContent,
-      mediaType: this.mediaType,
-      meta: this.meta && this.#encodeMetadata ? encodeObject(this.meta) : this.meta,
-      xkey: this.#xKey
-    })
-    const recoverScript = buildWitnessScript({
-      mediaContent: this.mediaContent,
-      mediaType: this.mediaType,
-      meta: this.meta && this.#encodeMetadata ? encodeObject(this.meta) : this.meta,
-      xkey: this.#xKey,
-      recover: true
+    const payment = createTransaction(Buffer.from(this.xKey, "hex"), "p2tr", this.network, {
+      scriptTree: this.taprootTree,
+      redeem: this.getRecoveryRedeemScript()
     })
 
-    if (!witnessScript || !recoverScript) {
-      throw new Error("Failed to build createRevealPsbt")
-    }
+    this.preview()
+    this.calculateNetworkFee()
+    this.preview({ activate: false })
 
-    const scriptTree: [Tapleaf, Tapleaf] = [
-      {
-        output: witnessScript
-      },
-      {
-        output: recoverScript
-      }
-    ]
-
-    const redeemScript = {
-      output: recoverScript,
-      redeemVersion: 192
-    }
-
-    const inscribePayTx = createTransaction(Buffer.from(this.#xKey, "hex"), "p2tr", this.network, {
-      scriptTree: scriptTree,
-      redeem: redeemScript
-    })
-
-    this.#inscribePayTx = inscribePayTx
-    this.#recovery = true
+    this.payment = payment
+    this.recovery = true
   }
 
   async isReady() {
-    if (!this.#commitAddress || !this.#feeForWitnessData) {
-      throw new Error("No commit address found. Please generate a commit address.")
-    }
+    this.isBuilt()
 
     if (!this.ready) {
       try {
@@ -315,45 +261,29 @@ export class OrdTransaction {
   }
 
   async fetchAndSelectSuitableUnspent() {
-    if (!this.#commitAddress || !this.#feeForWitnessData) {
-      throw new Error("No commit address found. Please generate a commit address.")
-    }
+    this.isBuilt()
 
-    const outAmount = this.#outs.reduce((acc, cur) => (acc += cur.value), 0)
-    const amount = this.postage + this.#feeForWitnessData! + outAmount
-
-    const utxos = await OrditApi.fetchSpendables({
-      address: this.#commitAddress,
-      value: convertSatoshisToBTC(amount),
-      network: this.network,
-      type: this.#safeMode === "on" ? "spendable" : "all"
-    })
-
-    const suitableUTXO = utxos.find((utxo) => utxo.sats >= amount)
-    if (!suitableUTXO) {
-      throw new Error("No suitable unspent found for reveal.")
-    }
-
-    this.#suitableUnspent = suitableUTXO
+    const amount = this.postage + this.fee + this.outputAmount
+    const [utxo] = await this.retrieveSelectedUTXOs(this.commitAddress!, amount)
+    this.suitableUnspent = utxo
     this.ready = true
 
-    return suitableUTXO
+    return this.suitableUnspent
   }
 }
 
 export type OrdTransactionOptions = Pick<GetWalletOptions, "safeMode"> & {
-  feeRate?: number
-  postage?: number
-  mediaType?: string
+  feeRate: number
+  postage: number
+  mediaType: string
   mediaContent: string
   destination: string
   changeAddress: string
   meta?: NestedObject
-  network?: Network
+  network: Network
   publicKey: string
   outs?: Outputs
   encodeMetadata?: boolean
-  enableRBF?: boolean
 }
 
 type Outputs = Array<{ address: string; value: number }>
