316a95b15f7d76aa48b32cf92aa9819dec51d9a7
KevinK <31565174+kevzzsk@users.noreply.github.com>
2023-12-20 14:37:24 +0800

    feat(sdk): Increase network fee limit to 5 BTC (#104)

--- packages/sdk/src/constants/index.ts
+++ packages/sdk/src/constants/index.ts
@@ -2,8 +2,8 @@
 // and majority of the miners don't pick txs w/ the following output value or lower
 export const MINIMUM_AMOUNT_IN_SATS = 600
 
-// Fee calculated by the fee estimator cannot be greater than 0.05 BTC in any case
-export const MAXIMUM_FEE = 5000000
+// Fee calculated by the fee estimator cannot be greater than 5 BTC in any case
+export const MAXIMUM_FEE = 500_000_000
 
 // Maximum number of bytes pushable to the witness stack
 export const MAXIMUM_SCRIPT_ELEMENT_SIZE = 520
